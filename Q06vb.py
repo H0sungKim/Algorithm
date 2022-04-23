d327ca59a5f7adb2501536850aaf2ed4c87b7c27
Hosung.Kim <hyongak516@mail.hongik.ac.kr>
2022-04-23 21:46:48 +0900

    .

---
+++ Q06vb.py
@@ -0,0 +1,12 @@
+'''
+====================
+Greedy
+Q6. 무지의 먹방 라이브
+--------------------
+문제 316p
+정답 513p
+====================
+'''
+
+# https://programmers.co.kr/learn/courses/30/lessons/42891?language=python3
+
---
+++ Q07vm.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2022 by Hosung.Kim <hyongak516@mail.hongik.ac.kr>
+
+'''
+====================
+Implementation
+Q7. 럭키 스트레이트
+--------------------
+문제 321p
+정답 515p
+====================
+'''
+
+n = input()
+length = len(n)
+scoreSum = 0
+
+for i in range(length//2) :
+    scoreSum += int(n[i])
+
+for i in range(length//2, length) :
+    scoreSum -= int(n[i])
+
+if scoreSum == 0 :
+    print("LUCKY")
+else :
+    print("READY")
