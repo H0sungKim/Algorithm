58279517d5de6f5003de7bd30d78f7dda226abe1
Hosung.Kim <hyongak516@mail.hongik.ac.kr>
2022-04-23 21:29:20 +0900

    .

---
+++ Q06vm.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2022 by Hosung.Kim <hyongak516@mail.hongik.ac.kr>
+
+'''
+====================
+Greedy
+Q6. 무지의 먹방 라이브
+--------------------
+문제 316p
+정답 513p
+====================
+'''
+
+# https://programmers.co.kr/learn/courses/30/lessons/42891?language=python3
+
+def solution(food_times, k):
+    food = food_times[:]
+    t = 0
+    while True :
+        t += 1
+        if k-len(food) < 0 :
+            break
+        k -= len(food)
+        food = [i for i in food if i != t]
+    for i in range(len(food_times)) :
+        if food_times[i] >= t :
+            if k == 0 :
+                return i+1
+            else :
+                k -= 1
+
+# print(solution([3, 1, 2], 3))
