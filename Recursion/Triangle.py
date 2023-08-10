00415792248ca1dd3d0dfebfc71efa227e8a02c8
H0sungKim <hyongak516@mail.hongik.ac.kr>
2023-08-10 16:39:58 +0900

    Create Recursion folder

---
+++ Recursion/Triangle.py
@@ -0,0 +1,28 @@
+# 2023 Hosung.Kim <hyongak516@mail.hongik.ac.kr>
+#
+# URL :           https://www.acmicpc.net/problem/2448
+# Difficulty :    골드IV
+# Problem :       별 찍기 - 11
+
+import copy
+
+def upgradeTriangle(repeatCount, triangle) :
+    if repeatCount == 0 :
+        return triangle
+    newTriangle = []
+    height = len(triangle)
+    for i in range(height) :
+        newTriangle.append(" "*height + triangle[i] + " "*height)
+    for i in range(height) :
+        newTriangle.append(triangle[i] + " " + triangle[i])
+        
+    return upgradeTriangle(repeatCount-1, newTriangle)
+    
+basicTriangle = ["  *  ", " * * ", "*****"]
+
+repeatCount = int(input())
+
+resultTriangle = upgradeTriangle(repeatCount, basicTriangle)
+
+for i in range(len(resultTriangle)) :
+    print(resultTriangle[i])
